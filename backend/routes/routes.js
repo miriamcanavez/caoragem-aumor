a659349c570d95f8b61034759d05b4255f1637d3
miriamcanavez <miriamcanavez@gmail.com>
2025-11-22 12:18:41 +0000

    Integração das rotas de adicionar e editar para consulta dos caes na base de dados. Adaptação das páginas para EJS.

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -38,18 +38,40 @@ router.get("/registarCao", (req, res) => {
 
 router.get("/registarCao/:id", async (req, res) => {
   const db = await connectDB();
-  const cao = await db.all(`
-    SELECT c.id_cao, c.nome, c.idade, c.sexo, ic.path_fotos 
+
+  const result = await db.all(`
+    SELECT c.id_cao, c.nome, c.idade, c.sexo, c.porte, c.info_medica, c.info_pessoal, c.status, ic.path_fotos 
     FROM caes c 
     INNER JOIN imagens_caes ic 
       ON c.id_cao = ic.id_cao 
     WHERE c.id_cao = ${req.params.id} AND ic.perfil = FALSE
   `);
 
+  if (!result || result.length === 0) {
+    return res.render("registarCao", { cao: null });
+  }
+
+  const fotos = result.map((r) => ({ path_fotos: r.path_fotos }));
+
+  while (fotos.length < 6) {
+    fotos.push({ path_fotos: "" });
+  }
+
+  const caoData = {
+    id_cao: result[0].id_cao,
+    nome: result[0].nome,
+    idade: result[0].idade,
+    sexo: result[0].sexo,
+    porte: result[0].porte,
+    info_medica: result[0].info_medica,
+    info_pessoal: result[0].info_pessoal,
+    status: result[0].status,
+
+    fotos: fotos,
+  };
+
   res.render("registarCao", {
-    cao: {
-      fotos: cao,
-    },
+    cao: caoData,
   });
 });
 
