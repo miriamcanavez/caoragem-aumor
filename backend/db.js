e3d1d12350f22967a191b1e6e141759d982bcb38
miriamcanavez <miriamcanavez@gmail.com>
2025-11-23 16:04:28 +0000

    Alteração no gerenciamento de conexão da base de dados para reuso da instância de conexão.

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,23 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
-
 import path from "path";
 import { fileURLToPath } from "url";
+
+let dbInstance = null;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dbPath = path.join(__dirname, "../db/caoragemaumor.db");
 
-async function connectDB() {
-  const db = await open({
+export default async function connectDB() {
+  if (dbInstance) return dbInstance;
+
+  dbInstance = await open({
     filename: dbPath,
     driver: sqlite3.Database,
   });
 
   console.log("📁 Base de dados SQLite conectada com sucesso");
 
-  return db;
+  return dbInstance;
 }
-
-export default connectDB;
