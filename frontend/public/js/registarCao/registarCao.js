29691e4b97e28cb7a6bec0fc73ea1465932e056a
miriamcanavez <miriamcanavez@gmail.com>
2025-11-29 11:58:55 +0000

    Inclusão do evento de remoção de imagens adicionadas.

--- frontend/public/js/registarCao/registarCao.js
+++ frontend/public/js/registarCao/registarCao.js
@@ -1,31 +1,38 @@
 const nomeInput = document.querySelector('input[name="nome"]');
 const hiddenNome = document.querySelector("#nomeCaoHidden");
 
+// Sincroniza input visível com hidden
 nomeInput.addEventListener("input", () => {
   hiddenNome.value = nomeInput.value;
 });
 
 document.querySelectorAll("img[data-index]").forEach((img) => {
-  img.addEventListener("click", () => {
-    const index = img.dataset.index;
-    const input = document.querySelector(
-      `input[type="file"][data-index="${index}"]`
-    );
-    input.click();
-  });
-});
+  const index = img.dataset.index;
+  const input = document.querySelector(
+    `input[type="file"][data-index="${index}"]`
+  );
+  const container = img.parentElement;
 
-document.querySelectorAll('input[type="file"][data-index]').forEach((input) => {
+  container.addEventListener("click", (e) => {
+    if (container.classList.contains("imagem-empty")) {
+      input.click();
+    } else {
+      input.value = "";
+      img.src = "/images/camera.png";
+      container.classList.remove("imagem-foto");
+      container.classList.add("imagem-empty");
+    }
+  });
+
   input.addEventListener("change", () => {
     const file = input.files[0];
     if (!file) return;
 
-    const index = input.dataset.index;
-    const img = document.querySelector(`img[data-index="${index}"]`);
-
     const reader = new FileReader();
     reader.onload = (e) => {
-      img.src = e.target.result; // preview da imagem
+      img.src = e.target.result;
+      container.classList.add("imagem-foto");
+      container.classList.remove("imagem-empty");
     };
 
     reader.readAsDataURL(file);
